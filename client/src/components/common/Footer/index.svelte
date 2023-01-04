be07fab91fa40f4d50b0e13099bb2acc8a6cf5f9
Kaym Kassai <kassai.kaym@gmail.com>
2023-01-04 08:03:25 +0100

    Home page design

---
+++ client/src/components/common/Footer/index.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+</script>
+
+<div class="mt-20 flex flex-col border-t-[1px] border-gray-200">
+  <footer class="footer p-10 bg-base-100 text-neutral">
+    <div>
+      <span class="footer-title">BeSitter</span> 
+      <a class="link link-hover">Comment ça marche</a> 
+      <a class="link link-hover">Aide</a> 
+      <a class="link link-hover">Tarifs</a>
+      <a class="link link-hover">À propos de nous</a>
+      <a class="link link-hover">Contact</a>
+    </div> 
+    <div>
+      <span class="footer-title">Découvrir</span> 
+      <a class="link link-hover">Conseils et articles</a> 
+      <a class="link link-hover">Confiance & sécurité</a> 
+      <a class="link link-hover">Les normes communautaires</a> 
+      <a class="link link-hover">Partenaires</a>
+    </div> 
+    <div>
+      <span class="footer-title">Légal</span> 
+      <a class="link link-hover">Confidentialité</a> 
+      <a class="link link-hover">Conditions</a>
+    </div> 
+    <div>
+      <span class="footer-title">Newsletter</span> 
+      <div class="form-control w-80">
+        <label class="label">
+          <span class="label-text">Enter your email address</span>
+        </label> 
+        <div class="relative">
+          <input type="text" placeholder="username@site.com" class="input input-bordered w-full pr-16" /> 
+          <button class="btn btn-primary absolute top-0 right-0 rounded-l-none">S'inscrire</button>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <div class="ocean">
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+  </div>
+</div>
+
+
+
+<style>
+  .ocean {
+  height: 80px; /* change the height of the waves here */
+  width: 100%;
+  position: relative;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  overflow-x: hidden;
+}
+
+.wave {
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 800 88.7'%3E%3Cpath d='M800 56.9c-155.5 0-204.9-50-405.5-49.9-200 0-250 49.9-394.5 49.9v31.8h800v-.2-31.6z' fill='%233eb9c0'/%3E%3C/svg%3E");
+  position: absolute;
+  width: 200%;
+  height: 100%;
+  animation: wave 10s -3s linear infinite;
+  transform: translate3d(0, 0, 0);
+  opacity: 0.8;
+}
+
+.wave:nth-of-type(2) {
+  bottom: 0;
+  animation: wave 18s linear reverse infinite;
+  opacity: 0.5;
+}
+
+.wave:nth-of-type(3) {
+  bottom: 0;
+  animation: wave 20s -1s linear infinite;
+  opacity: 0.5;
+}
+
+@keyframes wave {
+    0% {transform: translateX(0);}
+    50% {transform: translateX(-25%);}
+    100% {transform: translateX(-50%);}
+}
+
+</style>
--- client/src/components/home/Partners/index.svelte
+++ client/src/components/home/Partners/index.svelte
@@ -2,27 +2,117 @@
 
 </script>
 
-<div class="w-full h-14 bg-primary flex flex-row items-center justify-between gap-4 px-2 lg:px-16 overflow-hidden">
-	<div class="w-32 h-8 bg-white flex items-center justify-center">
-		<span class="text-sm font-bold">Partenaire 1</span>
-	</div>
-	<div class="w-32 h-8 bg-white flex items-center justify-center">
-		<span class="text-sm font-bold">Partenaire 2</span>
-	</div>
-	<div class="w-32 h-8 bg-white flex items-center justify-center">
-		<span class="text-sm font-bold">Partenaire 3</span>
-	</div>
-	<div class="w-32 h-8 bg-white flex items-center justify-center">
-		<span class="text-sm font-bold">Partenaire 4</span>
-	</div>
-	<div class="w-32 h-8 bg-white flex items-center justify-center">
-		<span class="text-sm font-bold">Partenaire 5</span>
-	</div>
-	<div class="w-32 h-8 bg-white flex items-center justify-center">
-		<span class="text-sm font-bold">Partenaire 6</span>
+<div class="slider">
+	<div class="slide-track">
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+		</div>
 	</div>
 </div>
 
-<style>
-	/* your styles go here */
+<style lang="scss">
+
+@mixin white-gradient {
+
+	background: linear-gradient(to right,  rgba(255,255,255,1) 0%,rgba(255,255,255,0) 100%);
+
+}
+
+$animationSpeed: 40s;
+
+// Animation
+@keyframes scroll {
+
+	0% { transform: translateX(0); }
+	100% { transform: translateX(calc(-250px * 7))}
+
+}
+
+
+// Styling
+.slider {
+
+	background: white;
+	box-shadow: 0 10px 20px -5px rgba(0, 0, 0, .125);
+	height: 100px;
+	margin: auto;
+	overflow:hidden;
+	position: relative;
+	width: 100%;
+	
+	&::before,
+	&::after {
+		@include white-gradient;
+		content: "";
+		height: 100px;
+		position: absolute;
+		width: 200px;
+		z-index: 2;
+	}
+	
+	&::after {
+		right: 0;
+		top: 0;
+		transform: rotateZ(180deg);
+	}
+
+	&::before {
+		left: 0;
+		top: 0;
+	}
+	
+	.slide-track {
+
+		animation: scroll $animationSpeed linear infinite;
+		display: flex;
+		width: calc(250px * 14);
+
+	}
+	
+	.slide {
+
+		height: 100px;
+		width: 250px;
+	
+	}
+}
 </style>
