77b142eb53cf168da6a6c5da52ee1c128580af1b
Kaym Kassai <kassai.kaym@gmail.com>
2023-01-06 13:33:25 +0100

    Update github action

--- client/src/components/home/Partners/index.svelte
+++ client/src/components/home/Partners/index.svelte
@@ -1,118 +1,180 @@
 <script lang="ts">
-
 </script>
 
 <div class="slider">
 	<div class="slide-track">
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
+				height="100"
+				width="250"
+				alt=""
+			/>
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
+				height="100"
+				width="250"
+				alt=""
+			/>
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
+				height="100"
+				width="250"
+				alt=""
+			/>
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
+				height="100"
+				width="250"
+				alt=""
+			/>
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
+				height="100"
+				width="250"
+				alt=""
+			/>
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
+				height="100"
+				width="250"
+				alt=""
+			/>
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
+				height="100"
+				width="250"
+				alt=""
+			/>
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
+				height="100"
+				width="250"
+				alt=""
+			/>
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
+				height="100"
+				width="250"
+				alt=""
+			/>
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
+				height="100"
+				width="250"
+				alt=""
+			/>
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
+				height="100"
+				width="250"
+				alt=""
+			/>
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
+				height="100"
+				width="250"
+				alt=""
+			/>
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
+				height="100"
+				width="250"
+				alt=""
+			/>
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
+				height="100"
+				width="250"
+				alt=""
+			/>
 		</div>
 	</div>
 </div>
 
 <style lang="scss">
+	@mixin white-gradient {
+		background: linear-gradient(to right, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 100%);
+	}
 
-@mixin white-gradient {
+	$animationSpeed: 40s;
 
-	background: linear-gradient(to right,  rgba(255,255,255,1) 0%,rgba(255,255,255,0) 100%);
+	// Animation
+	@keyframes scroll {
+		0% {
+			transform: translateX(0);
+		}
+		100% {
+			transform: translateX(calc(-250px * 7));
+		}
+	}
 
-}
-
-$animationSpeed: 40s;
-
-// Animation
-@keyframes scroll {
-
-	0% { transform: translateX(0); }
-	100% { transform: translateX(calc(-250px * 7))}
-
-}
-
-
-// Styling
-.slider {
-
-	background: white;
-	box-shadow: 0 10px 20px -5px rgba(0, 0, 0, .125);
-	height: 100px;
-	margin: auto;
-	overflow:hidden;
-	position: relative;
-	width: 100%;
-	
-	&::before,
-	&::after {
-		@include white-gradient;
-		content: "";
+	// Styling
+	.slider {
+		background: white;
+		box-shadow: 0 10px 20px -5px rgba(0, 0, 0, 0.125);
 		height: 100px;
-		position: absolute;
-		width: 200px;
-		z-index: 2;
-	}
-	
-	&::after {
-		right: 0;
-		top: 0;
-		transform: rotateZ(180deg);
-	}
+		margin: auto;
+		overflow: hidden;
+		position: relative;
+		width: 100%;
 
-	&::before {
-		left: 0;
-		top: 0;
-	}
-	
-	.slide-track {
+		&::before,
+		&::after {
+			@include white-gradient;
+			content: '';
+			height: 100px;
+			position: absolute;
+			width: 200px;
+			z-index: 2;
+		}
 
-		animation: scroll $animationSpeed linear infinite;
-		display: flex;
-		width: calc(250px * 14);
+		&::after {
+			right: 0;
+			top: 0;
+			transform: rotateZ(180deg);
+		}
 
-	}
-	
-	.slide {
+		&::before {
+			left: 0;
+			top: 0;
+		}
 
-		height: 100px;
-		width: 250px;
-	
+		.slide-track {
+			animation: scroll $animationSpeed linear infinite;
+			display: flex;
+			width: calc(250px * 14);
+		}
+
+		.slide {
+			height: 100px;
+			width: 250px;
+		}
 	}
-}
 </style>
